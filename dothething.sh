edd773e43eea2b5e839372e65dbeb8d987fc33c9
fakuivan <fakuivan@gmail.com>
2020-08-10 09:46:51 -0300

    Rename files to correct extension

--- dothething.sh
+++ dothething.sh
@@ -5,7 +5,7 @@ min() {
 }
 
 LOGS_DIR="./logs"
-NAMES=( "datosDemandasPreDes__ChtDemandaArea5Min_1002__$(date +%Y-%m-%d_%H-%M-%S).svg" );
+NAMES=( "datosDemandasPreDes__ChtDemandaArea5Min_1002__$(date +%Y-%m-%d_%H-%M-%S).csv" );
 URLS=( "https://aplic.cammesa.com/complemento-portal/descargar?type=csv&value=ChtDemandaArea5Min_1002.xml&header=datosDemandasPreDes" );
 
 ELEMENTS="$(min "${#NAMES[@]}" "${#URLS[@]}")"
